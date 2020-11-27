35c55e95c5ad76389a7c0fb3e93cc16cea48c9a9
심승용 <seungyong.shim@mirero.co.kr>
2020-11-27 19:21:38 +0900

    수정

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,21 @@
+import akka.actor.ActorSystem
+import akka.actor.Props
+import akka.actor.UntypedAbstractActor
+import akka.actor.setup.ActorSystemSetup
+
 fun main(args: Array<String>) {
-    println("Hello World!")
+    val actorSystemSetup = ActorSystemSetup.create()
+    val actorSystem = ActorSystem.create("hello", actorSystemSetup);
+    val pingActor = actorSystem.actorOf(Props.create { PingActor() })
+    pingActor.tell("Hello World", null)
+    actorSystem.terminate()
 }
+
+class PingActor : UntypedAbstractActor() {
+    override fun onReceive(message: Any?) {
+        context.system.log().info("$message")
+    }
+}
+
+
+
